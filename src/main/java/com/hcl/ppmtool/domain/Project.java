91ad1f85fe208b6a0d7692873b5fdcfc2fe6075e
cn <contact@nguyenc.com>
2022-01-20 20:56:52 -0500

    Use lombok for Project.java

--- src/main/java/com/hcl/ppmtool/domain/Project.java
+++ src/main/java/com/hcl/ppmtool/domain/Project.java
@@ -1,13 +1,19 @@
 package com.hcl.ppmtool.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.sun.org.apache.xpath.internal.objects.XString;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Project {
     @Id
@@ -16,8 +22,8 @@ public class Project {
     @NotBlank(message = "Project name is required")
     private String projectName;
     @NotBlank(message = "Project Identifier is required")
-    @Size(min = 4,max = 5,message = "Please use 4 to 5 characters")
-    @Column(updatable = false,unique = true)
+    @Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
     @NotBlank(message = "Project description is required")
     private String description;
@@ -39,71 +45,71 @@ public class Project {
     protected void onUpdate() {
         this.updated_At = new Date();
     }
-
-    public Project() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getProjectName() {
-        return projectName;
-    }
-
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
-    }
-
-    public String getProjectIdentifier() {
-        return projectIdentifier;
-    }
-
-    public void setProjectIdentifier(String projectIdentifier) {
-        this.projectIdentifier = projectIdentifier;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Date getStart_date() {
-        return start_date;
-    }
-
-    public void setStart_date(Date start_date) {
-        this.start_date = start_date;
-    }
-
-    public Date getEnd_date() {
-        return end_date;
-    }
-
-    public void setEnd_date(Date end_date) {
-        this.end_date = end_date;
-    }
-
-    public Date getCreated_At() {
-        return created_At;
-    }
-
-    public void setCreated_At(Date created_At) {
-        this.created_At = created_At;
-    }
-
-    public Date getUpdated_At() {
-        return updated_At;
-    }
-
-    public void setUpdated_At(Date updated_At) {
-        this.updated_At = updated_At;
-    }
+//
+//    public Project() {
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getProjectName() {
+//        return projectName;
+//    }
+//
+//    public void setProjectName(String projectName) {
+//        this.projectName = projectName;
+//    }
+//
+//    public String getProjectIdentifier() {
+//        return projectIdentifier;
+//    }
+//
+//    public void setProjectIdentifier(String projectIdentifier) {
+//        this.projectIdentifier = projectIdentifier;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+//
+//    public Date getStart_date() {
+//        return start_date;
+//    }
+//
+//    public void setStart_date(Date start_date) {
+//        this.start_date = start_date;
+//    }
+//
+//    public Date getEnd_date() {
+//        return end_date;
+//    }
+//
+//    public void setEnd_date(Date end_date) {
+//        this.end_date = end_date;
+//    }
+//
+//    public Date getCreated_At() {
+//        return created_At;
+//    }
+//
+//    public void setCreated_At(Date created_At) {
+//        this.created_At = created_At;
+//    }
+//
+//    public Date getUpdated_At() {
+//        return updated_At;
+//    }
+//
+//    public void setUpdated_At(Date updated_At) {
+//        this.updated_At = updated_At;
+//    }
 }
