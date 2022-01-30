5c1ff27151cea876c1cdb0bab6333c59198a4746
congnguyenhcl <96488290+congnguyenhcl@users.noreply.github.com>
2022-01-29 23:23:35 -0500

    branch70 - Users can only look up their own project tasks and backlogs

--- src/main/java/com/hcl/ppmtool/services/ProjectTaskService.java
+++ src/main/java/com/hcl/ppmtool/services/ProjectTaskService.java
@@ -25,12 +25,15 @@ public class ProjectTaskService {
     @Autowired
     private ProjectRepository projectRepository;
 
+    @Autowired
+    private ProjectService projectService;
 
-    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
 
-        try {
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask, String username){
+
+
             //PTs to be added to a specific project, project != null, BL exists
-            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+            Backlog backlog =  projectService.findProjectByIdentifier(projectIdentifier, username).getBacklog(); //backlogRepository.findByProjectIdentifier(projectIdentifier);
             //set the bl to pt
             System.out.println(backlog);
             projectTask.setBacklog(backlog);
@@ -42,73 +45,70 @@ public class ProjectTaskService {
             backlog.setPTSequence(BacklogSequence);
 
             //Add Sequence to Project Task
-            projectTask.setProjectSequence(backlog.getProjectIdentifier() + "-" + BacklogSequence);
+            projectTask.setProjectSequence(backlog.getProjectIdentifier()+"-"+BacklogSequence);
             projectTask.setProjectIdentifier(projectIdentifier);
 
             //INITIAL priority when priority null
 
             //INITIAL status when status is null
-            if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+            if(projectTask.getStatus()==""|| projectTask.getStatus()==null){
                 projectTask.setStatus("TO_DO");
             }
-            //Fix bug with priority in Spring Boot Server, needs to check null first
 
-            if (projectTask.getPriority() == 0 || projectTask.getPriority() == null) { //In the future we need
-                // projectTask.getPriority()== 0 to handle the form
+            //Fix bug with priority in Spring Boot Server, needs to check null first
+            if(projectTask.getPriority()==null||projectTask.getPriority()==0){ //In the future we need projectTask.getPriority()== 0 to handle the form
                 projectTask.setPriority(3);
             }
 
             return projectTaskRepository.save(projectTask);
-        } catch (Exception e) {
-            throw new ProjectNotFoundException("Project not Found");
-        }
+
 
     }
 
-    public Iterable<ProjectTask> findBacklogById(String id) {
+    public Iterable<ProjectTask>findBacklogById(String id, String username){
 
-        Project project = projectRepository.findByProjectIdentifier(id);
-
-        if (project == null) {
-            throw new ProjectNotFoundException("Project with ID: '" + id + "' does not exist");
-        }
+        projectService.findProjectByIdentifier(id, username);
 
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
 
-    public ProjectTask findPTByProjectSequence(String backlog_id, String pt_id) {
 
-        //make sure we are searching on the existing backlog
+    public ProjectTask findPTByProjectSequence(String backlog_id, String pt_id){
+
+        //make sure we are searching on an existing backlog
         Backlog backlog = backlogRepository.findByProjectIdentifier(backlog_id);
-        if (backlog == null) {
-            throw new ProjectNotFoundException("Project with ID: '" + backlog_id + "' does not exist");
+        if(backlog==null){
+            throw new ProjectNotFoundException("Project with ID: '"+backlog_id+"' does not exist");
         }
 
         //make sure that our task exists
         ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
 
-        if (projectTask == null) {
-            throw new ProjectNotFoundException("Project Task '" + pt_id + "' not found");
+        if(projectTask == null){
+            throw new ProjectNotFoundException("Project Task '"+pt_id+"' not found");
         }
 
         //make sure that the backlog/project id in the path corresponds to the right project
-        if (!projectTask.getProjectIdentifier().equals(backlog_id)) {
-            throw new ProjectNotFoundException("Project Task '" + pt_id + "' does not exist in project: '" + backlog_id);
+        if(!projectTask.getProjectIdentifier().equals(backlog_id)){
+            throw new ProjectNotFoundException("Project Task '"+pt_id+"' does not exist in project: '"+backlog_id);
         }
+
+
         return projectTask;
     }
 
-    public ProjectTask updateByProjectSequence(ProjectTask updatedTask, String backlog_id, String pt_id) {
+    public ProjectTask updateByProjectSequence(ProjectTask updatedTask, String backlog_id, String pt_id){
         ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
+
         projectTask = updatedTask;
 
         return projectTaskRepository.save(projectTask);
     }
 
-    public void deletePTByProjectSequence(String backlog_id, String pt_id) {
+
+    public void deletePTByProjectSequence(String backlog_id, String pt_id){
         ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
-
-
         projectTaskRepository.delete(projectTask);
     }
+
 }
